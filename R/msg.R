33854937921736aee5fbde7d5091ea42795d996c
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-08-15 21:46:48 +0200

    Vectorize msg()

--- R/msg.R
+++ R/msg.R
@@ -1,11 +1,18 @@
 #' Find message string
 #'
-#' @param msgcode Integer with message code
-#' @return A string with the message code
+#' @param msgcode Integer vector with message codes
+#' @return A vector of strings with the message code
 #' @examples
-#' msg(31)
+#' msg(c(31, 41))
 #' @export
 msg <- function(msgcode) {
   mess <- growthscreener::messages_hgt
-  mess[mess[, "msgcode"] == msgcode, "msg"]
+  result <- character(length(msgcode))
+  if (length(result) == 0) return(return)
+  for (i in 1L:length(msgcode)) {
+    m <- mess[mess[, "msgcode"] == msgcode[i], "msg"]
+    if (length(m) == 0) m <- ""
+    result[i] <- m
+  }
+  result
 }
