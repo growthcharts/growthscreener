ecb0eb15f0982cf7bd05a56129b518c1c987bfb3
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-07-14 09:02:05 +0200

    Update messages

--- data-raw/R/messages.R
+++ data-raw/R/messages.R
@@ -1,7 +1,3 @@
-path <- path.expand("~/Package/growthscreener/growthscreener")
-
-# Added message 20 by hand on 2019-08-12, so we need to work for
-# this file now.
-messages <- read.table(file = file.path(path, "data-raw", "data", "messages.txt"),
+messages <- read.table(file = file.path("data-raw", "data", "messages.txt"),
                   sep = "\t", header = TRUE, stringsAsFactors = FALSE)
 usethis::use_data(messages, overwrite = TRUE)
