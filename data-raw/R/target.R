b7e19689d73984283126c025dfb8be3a24391812
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-12-27 15:19:13 +0100

    Update built-in target

--- data-raw/R/target.R
+++ data-raw/R/target.R
@@ -1,3 +1,3 @@
-fn <- system.file("examples", "Laura_S.json", package = "bdsreader")
+fn <- system.file("extdata/bds_v2.0/smocc/Laura_S.json", package = "jamesdemodata")
 target <- bdsreader::read_bds(fn)
 usethis::use_data(target, overwrite = TRUE)
