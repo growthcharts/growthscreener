6ec4b7f9714d7118ab4992632df3918f30110276
Stef van Buuren <stef.vanbuuren@tno.nl>
2023-03-29 23:03:05 +0200

    Make sure ga is completed weeks

--- R/calculate_advice_wgt.R
+++ R/calculate_advice_wgt.R
@@ -49,8 +49,8 @@ calculate_advice_wgt  <- function(sex = NA_character_,
   if (any(nchar(dom) >= 8 & !is.na(dom))) age <- date2age(dob, dom) else age <- dom
   if (any(nchar(dom_hgt) >= 8 & !is.na(dom_hgt))) age_hgt <- date2age(dob, dom_hgt) else age_hgt <- dom_hgt
 
-  # convert ga
-  if(ga > 60 & !is.na(ga)) ga <- ga/7 # convert days to weeks
+  # convert days to completed weeks
+  if (ga > 60 && !is.na(ga)) ga <- as.integer(ga / 7)
 
   # sort wgt and hgt observations
   df <- data.frame(age = age, y = y) %>%
